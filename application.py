18f339720fd4f2b6aef5f171f03de35ec18624f9
bhalla02 <sarthakbhalla723@gmail.com>
2024-07-04 16:53:43 +0530

    my commit

--- application.py
+++ application.py
@@ -3,7 +3,8 @@ from flask import Flask, request, jsonify,render_template
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
-app = Flask(__name__)
+application = Flask(__name__)
+app = application
 
 
 ##import  rideg regressor model and standard scaler model
